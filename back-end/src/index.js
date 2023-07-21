b1e5d28c4277e6e1224e6a6c556bc1b8b05a47e1
FedeVazquez <fedenv2009@hotmail.com>
2023-07-21 15:36:45 -0300

    Crea variable de entorno con la url del servidor dentro del proyecto de React.

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mainRoutes = require("./routes/mainRoutes");
 
-const port = 3000;
+const port = 3001;
 app.use(express.json());
 
 app.listen(port, () => {
