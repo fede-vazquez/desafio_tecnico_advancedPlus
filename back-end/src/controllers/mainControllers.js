1e50d6efee0f2dc6d5503812b5ae01dd6499498e
fede-vazquez <fedenv2009@hotmail.com>
2023-07-22 14:36:06 -0300

    Arregla los nombres de los atributos que tiene el objeto "newUser" que se crea en el controlador de "register" para que se guarde correctamente el usuario en la db. Crea controlador y ruta para encontrar un usuario especifico por su id.

--- back-end/src/controllers/mainControllers.js
+++ back-end/src/controllers/mainControllers.js
@@ -2,34 +2,60 @@ const db = require("../../database/models");
 const { v4: uuidV4 } = require("uuid");
 
 module.exports = {
+  /**
+   * Controlador que devuelve todos los usuarios.
+   */
   getUsers: async (req, res) => {
     try {
       const users = await db.Users.findAll();
       res.json({ status: 200, data: users });
     } catch (error) {
       console.log(error);
-      res.json({ status: 404, msg: "Ocurrió un error con el servidor" });
+      res.json({ status: 503, msg: "Ocurrió un error con el servidor" });
     }
   },
 
+  /**
+   * Controlador que devuelve un usuario en base al id que viaja por el query parameter.
+   * @param {String} req.params.id Id del usuario que viaja dentro del query parameter.
+   */
+  getOneUser: async (req, res) => {
+    try {
+      const user = await db.Users.findByPk(req.params.id, { include: ["rol"] });
+      res.json({ status: 200, data: user });
+    } catch (error) {
+      console.log(error);
+      res.json({ status: 503, msg: "Ocurrió un error con el servidor" });
+    }
+  },
+
+  /**
+   * Controlador que registra un usuario.
+   *
+   * Toma del body las propiedades: firstName, lastName, password, email y dni.
+   *
+   * Y toma del FTP el nombre del archivo guardado del avatar.
+   */
   register: async (req, res) => {
     /**
      * Objeto que contiene los datos que se cargarán en la db.
      * @type {Object} newUser Datos del nuevo usuario.
      * @property {String} id Id del usuario, creado por uuidV4.
-     * @property {String} firstName Nombre del usuario.
-     * @property {String} lastName Apellido del usuario.
+     * @property {String} first_name Nombre del usuario.
+     * @property {String} last_name Apellido del usuario.
+     * @property {String} email Email del usuario.
      * @property {String} password Contraseña del usuario, con hash.
      * @property {String} avatar Ruta al avatar del usuario.
      * @property {Number} dni dni del usuario.
      */
     const newUser = {
       id: uuidV4(),
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      email: req.body.email,
       password: "hash",
       avatar: "img",
-      dni: req.body.dni,
+      dni: Number(req.body.dni),
     };
 
     try {
@@ -40,7 +66,7 @@ module.exports = {
       });
     } catch (error) {
       console.log(error);
-      res.json({ status: 404, msg: "Ocurrió un error con el servidor" });
+      res.json({ status: 503, msg: "Ocurrió un error con el servidor" });
     }
   },
 };
--- back-end/src/routes/mainRoutes.js
+++ back-end/src/routes/mainRoutes.js
@@ -3,6 +3,7 @@ const router = Router();
 const mainControllers = require("../controllers/mainControllers");
 
 router.get("/users", mainControllers.getUsers);
+router.get("/users/detail/:id", mainControllers.getOneUser);
 
 router.post("/users/register", mainControllers.register);
 
