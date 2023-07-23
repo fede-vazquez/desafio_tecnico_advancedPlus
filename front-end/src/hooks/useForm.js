cd67fef0b6fcfa5ba91c1d17a3bacf28b8ff0c20
fede-vazquez <fedenv2009@hotmail.com>
2023-07-23 20:16:53 -0300

    Crea hook personalizado para trabajar con el formulario.

---
+++ front-end/src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+/**
+ * Hook personalizado para trabajar con el formulario.
+ * @param {Object} formData Fatos iniciales del formulario.
+ */
+function useForm({ formData }) {
+  const [form, setForm] = useState(formData);
+
+  /**
+   * Función para controlar cuando se modifica un input.
+   * @param {String} nameInput Nombre del atributo que estará dentro del form.
+   * @param {String|Number} value Valor del input.
+   */
+  function handleChange(nameInput, value) {
+    setForm((prevForm) => {
+      return { ...prevForm, [nameInput]: value };
+    });
+  }
+
+  return { form, handleChange };
+}
+
+export default useForm;
